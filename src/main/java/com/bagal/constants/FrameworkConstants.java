885d9fe498e61b80fd289d62d6dff9c1b7b62b05
bagalumaji <bagalumaji@gmail.com>
2023-10-03 09:21:56 +0530

    added to store the constants

--- src/main/java/com/bagal/constants/FrameworkConstants.java
+++ src/main/java/com/bagal/constants/FrameworkConstants.java
@@ -1,4 +1,10 @@
 package com.bagal.constants;
 
-public class FrameworkConstants {
+import lombok.Getter;
+
+public final class FrameworkConstants {
+    private FrameworkConstants(){}
+    private static final @Getter String basePath = System.getProperty("user.dir");
+    private static final @Getter String basePathOfConfigs = basePath + "/src/test/resources/configs";
+    private static final @Getter String extentReportPath = basePath + "/target/index.html";
 }
