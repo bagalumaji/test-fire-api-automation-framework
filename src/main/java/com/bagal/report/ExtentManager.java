760feaee1c2c13ab1a8cc4ff3b75196003f28c31
bagalumaji <bagalumaji@gmail.com>
2023-10-03 09:21:33 +0530

    added reports

--- src/main/java/com/bagal/report/ExtentManager.java
+++ src/main/java/com/bagal/report/ExtentManager.java
@@ -1,9 +1,25 @@
 package com.bagal.report;
 
+import com.aventstack.extentreports.ExtentTest;
+
+import java.util.Objects;
+
 public final class ExtentManager {
     private ExtentManager(){}
 
-    public static void getExtentTest(){
 
+    private static final ThreadLocal<ExtentTest> EXTENT_TEST_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static ExtentTest getExtentTest(){
+        return EXTENT_TEST_THREAD_LOCAL.get();
+    }
+
+    static void setExtentTest(ExtentTest extentTest){
+        if(Objects.nonNull(extentTest)){
+            EXTENT_TEST_THREAD_LOCAL.set(extentTest);
+        }
+    }
+    public static void unload(){
+        EXTENT_TEST_THREAD_LOCAL.remove();
     }
 }
--- src/main/java/com/bagal/report/ExtentReport.java
+++ src/main/java/com/bagal/report/ExtentReport.java
@@ -1,4 +1,37 @@
 package com.bagal.report;
 
-public class ExtentReport {
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.bagal.constants.FrameworkConstants;
+import com.bagal.utils.CurrentEnvironmentDataUtils;
+
+import java.util.Objects;
+
+public final class ExtentReport {
+    private ExtentReport(){}
+    private static ExtentReports extentReports;
+    public static void initReport(){
+        if(Objects.isNull(extentReports)) {
+            extentReports = new ExtentReports();
+            ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(FrameworkConstants.getExtentReportPath());
+            extentSparkReporter.config().setReportName("Api Automation Report");
+            extentSparkReporter.config().setDocumentTitle("Test Fire Api Report");
+            extentSparkReporter.config().setTheme(Theme.DARK);
+            extentSparkReporter.config().setEncoding("uft-8");
+            extentReports.attachReporter(extentSparkReporter);
+            extentReports.setSystemInfo("Environment", CurrentEnvironmentDataUtils.getCurrentEnvironment());
+            extentReports.setSystemInfo("Automation Engineer","Umaji Bagal");
+        }
+    }
+
+    public static void flushReport(){
+        if(Objects.nonNull(extentReports)){
+            extentReports.flush();
+            ExtentManager.unload();
+        }
+    }
+    public static void createTest(String testCaseName){
+         ExtentManager.setExtentTest(extentReports.createTest(testCaseName));
+    }
 }
