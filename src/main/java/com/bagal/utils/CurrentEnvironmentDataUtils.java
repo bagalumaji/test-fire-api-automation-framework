4c42e7612969760be7d88aca84f246df6c3d15f4
bagalumaji <bagalumaji@gmail.com>
2023-10-03 09:20:43 +0530

    added for get the current env data

---
+++ src/main/java/com/bagal/utils/CurrentEnvironmentDataUtils.java
@@ -0,0 +1,43 @@
+package com.bagal.utils;
+
+import com.bagal.constants.FrameworkConstants;
+import com.bagal.exceptions.InvalidPathForJsonFileException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+public final class CurrentEnvironmentDataUtils {
+    private CurrentEnvironmentDataUtils() {
+    }
+
+    private static final Map<String, String> MAP;
+
+    @Getter
+    private static String currentEnvironment;
+
+    static {
+        currentEnvironment = System.getProperty("environment");
+        currentEnvironment = currentEnvironment == null ? "qa" : currentEnvironment.toLowerCase();
+        String fileName = FrameworkConstants.getBasePathOfConfigs() + "/" + "config-" + currentEnvironment + ".json";
+        try {
+            MAP = new ObjectMapper().readValue(new File(fileName), Map.class);
+        } catch (InvalidPathForJsonFileException | IOException e) {
+            throw new InvalidPathForJsonFileException("Config file not found at this location : " + fileName);
+        }
+    }
+
+    public static String getBaseUri() {
+        return MAP.get("baseuri");
+    }
+
+    public static String getUserName() {
+        return MAP.get("username");
+    }
+
+    public static String getPassword() {
+        return MAP.get("password");
+    }
+}
