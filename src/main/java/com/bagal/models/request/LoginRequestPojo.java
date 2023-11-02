a8c1e0dc813056233340450ee697e8580213823b
bagalumaji <bagalumaji@gmail.com>
2023-11-02 21:41:53 +0530

    updated pojo

--- src/main/java/com/bagal/models/request/LoginRequestPojo.java
+++ src/main/java/com/bagal/models/request/LoginRequestPojo.java
@@ -1,15 +1,26 @@
 package com.bagal.models.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class LoginRequestPojo {
     private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String password;
 }
